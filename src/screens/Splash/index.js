8f3a0dc7e04f2ff57772cd053f681f5550cc94bb
Hegefix <hedgefix@gmail.com>
2020-03-10 17:21:30 +0200

    implement navigation

---
+++ src/screens/Splash/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import styles from './styles';
+
+const SplashScreen = () => (
+  <View style={styles.container}>
+    <Text style={styles.titleText}>PET SHOP</Text>
+  </View>
+);
+
+export default SplashScreen;
