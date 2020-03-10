d5774cfee9f81a8f4d1dc152bc957e3a2ca38b99
Hegefix <hedgefix@gmail.com>
2020-03-10 15:02:36 +0200

    module resolver alias

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,22 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      require.resolve('babel-plugin-module-resolver'),
+      {
+        root: ['./src'],
+        alias: {
+          '@components': './src/components',
+          '@constants': './src/constants',
+          '@screens': './src/screens',
+          '@services': './src/services',
+          '@store': './src/store',
+          '@actions': './src/store/actions',
+          '@reducers': './src/store/reducers',
+          '@sagas': './src/store/sagas',
+          '@utils': './src/utils',
+        },
+      },
+    ],
+  ],
 };
